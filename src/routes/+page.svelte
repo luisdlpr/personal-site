03640f023ab38f4eeaa3259f7ca53b1b9a87946c
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-08-11 17:45:34 +1000

    playing around with lighting (with comments)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,8 @@
 		let renderer: THREE.WebGLRenderer;
 		let ambientLight: THREE.AmbientLight;
 		let directionalLight: THREE.DirectionalLight;
-		let spotLight: THREE.SpotLight;
+		// let spotLight: THREE.SpotLight;
+		// let pointLight: THREE.PointLight;
 		let controls: THREE.OrbitControls;
 		let model: any;
 		// let cube: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
@@ -34,44 +35,58 @@
 			scene = new THREE.Scene();
 			camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-			renderer = new THREE.WebGLRenderer();
+			renderer = new THREE.WebGLRenderer({ antialias: true });
 			renderer.setSize(window.innerWidth, window.innerHeight);
+			renderer.setPixelRatio(window.devicePixelRatio);
+			renderer.toneMapping = THREE.ACESFilmicToneMapping;
+			renderer.outputEncoding = THREE.sRGBEncoding;
+			renderer.gammaOutput = true;
+			renderer.shadowMap.enabled = true;
+			renderer.useLegacyLights = false;
 			document.body.appendChild(renderer.domElement);
 
+			// pointLight = new THREE.PointLight(0x00ff00, 1, 100, 0);
+			// pointLight.position.set(1, 0, 0);
+
+			// // const plHelper = new THREE.PointLightHelper(pointLight, 1);
+			// scene.add(pointLight);
+
 			// ambient light which is for the whole scene
-			ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+			ambientLight = new THREE.AmbientLight(0xfff2cc, 0.3);
 			ambientLight.castShadow = true;
 			scene.add(ambientLight);
 
-			directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+			directionalLight = new THREE.DirectionalLight(0xffffff, 3);
 			directionalLight.castShadow = true;
-			directionalLight.position.set(0, 3, 0);
+			directionalLight.position.set(3, 3, 0);
 
 			const dlHelper = new THREE.DirectionalLightHelper(directionalLight, 3);
 			scene.add(directionalLight, dlHelper);
 
-			spotLight = new THREE.SpotLight(0x00f00, 1, 8, Math.PI / 8, 0);
-			spotLight.castShadow = true;
-			spotLight.position.set(0, 3, 0);
-			const slHelper = new THREE.SpotLightHelper(spotLight, 3);
-			scene.add(spotLight, slHelper);
+			// spotLight = new THREE.SpotLight(0x00ff00, 10000, 8, Math.PI / 4, 0.5, 0);
+			// spotLight.castShadow = true;
+			// spotLight.position.set(1, 1, 0);
+			// const slHelper = new THREE.SpotLightHelper(spotLight, 3);
+			// scene.add(spotLight, slHelper);
+
+			// let spotLightTarget = new THREE.Object3D();
+			// spotLightTarget.position.set(1, -1, 0);
+			// scene.add(spotLightTarget);
+			// spotLight.target = spotLightTarget;
 
 			controls = new OrbitControls(camera, renderer.domElement);
 
 			//controls.update() must be called after any manual changes to the camera's transform
 			controls.update();
 
-			ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-			ambientLight.castShadow = true;
-			scene.add(ambientLight);
-
 			const loader = new GLTFLoader();
 			loader.load(
-				'src/assets/scene.gltf',
+				'./assets/chipboy/ChipBoy_SF.gltf',
 				function (gltf: InitGLTFJSON) {
 					console.log(gltf);
 					model = gltf.scene;
 					model.castShadow = true;
+					model.recieveShadow = true;
 					scene.add(model);
 				},
 				(xhr: OnLoadXHR) => {
