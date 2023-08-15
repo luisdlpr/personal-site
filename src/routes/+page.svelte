b60eb0dbc63dd47134dfc2d111c939f4ca511d68
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-08-16 09:45:47 +1000

    basic title, want css transitions and unsure about layout for now. should also define its own component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import ThreejsScene from '$lib/ChipBoy.svelte';
+	import '@fontsource/roboto';
 </script>
 
 <svelte:head>
@@ -7,7 +8,35 @@
 </svelte:head>
 
 <body>
-	<ThreejsScene --height="500px" />
+	<section id="title">
+		<h1>Luis Vicente Reyes</h1>
+		<h2>Software/Web Developer - Sydney NSW</h2>
+	</section>
 
-	<section>Hello World.</section>
+	<section>
+		<ThreejsScene --height="500px" />
+	</section>
 </body>
+
+<style>
+	:global(*) {
+		color: #e5e7eb;
+		font-family: sans-serif;
+		font-weight: 100;
+	}
+
+	#title {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		gap: 2px;
+	}
+
+	#title > * {
+		margin: 5px;
+	}
+
+	#title > h1 {
+		font-size: 5em;
+	}
+</style>
