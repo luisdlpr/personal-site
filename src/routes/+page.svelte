cb6a2bb4d75dd1249e93b21ff301458de9895d8c
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-08-17 00:10:25 +1000

    loading indicator propagated up to whole page using svelte store, needs alot of cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	import ThreejsScene from '$lib/ChipBoy.svelte';
+	import { writable } from 'svelte/store';
+	import '$lib/global.css';
 	import '@fontsource/roboto';
+
+	let main;
+	let loadingContainer;
+	let loading;
+
+	const progress = writable(0);
+
+	const setLoadingComplete = () => {
+		loadingContainer.style.display = 'none';
+		main.style.display = 'block';
+	};
+
+	progress.subscribe((value) => {
+		if (value >= 100) {
+			setLoadingComplete();
+		}
+	});
 </script>
 
 <svelte:head>
@@ -8,23 +27,22 @@
 </svelte:head>
 
 <body>
-	<section id="title">
-		<h1>Luis Vicente Reyes</h1>
-		<h2>Software/Web Developer - Sydney NSW</h2>
-	</section>
+	<main style="display: none;" bind:this={main}>
+		<section id="title">
+			<h1>Luis Vicente Reyes</h1>
+			<h2>Software/Web Developer - Sydney NSW</h2>
+		</section>
 
-	<section>
-		<ThreejsScene --height="500px" />
-	</section>
+		<section>
+			<ThreejsScene --height="500px" progress={(newProgress) => ($progress = newProgress)} />
+		</section>
+	</main>
+	<div bind:this={loadingContainer}>
+		<progress value={$progress} max="100" />
+	</div>
 </body>
 
 <style>
-	:global(*) {
-		color: #e5e7eb;
-		font-family: sans-serif;
-		font-weight: 100;
-	}
-
 	#title {
 		display: flex;
 		align-items: center;
