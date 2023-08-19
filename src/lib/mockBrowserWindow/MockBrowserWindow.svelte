32ea26d512a891b976a271ae90a25622037c7f7a
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-08-19 10:40:28 +1000

    layout fine tuning and abstraction of mock window to a component

---
+++ src/lib/mockBrowserWindow/MockBrowserWindow.svelte
@@ -0,0 +1,47 @@
+<main id="window">
+	<div id="windowTopDecoration">
+		<div id="exit" class="circleButton" />
+		<div id="min" class="circleButton" />
+		<div id="max" class="circleButton" />
+	</div>
+	<slot name="content" />
+</main>
+
+<style>
+	#window {
+		display: flex;
+		flex-direction: column;
+		border-radius: 5px;
+		background-color: var(--slate800);
+		filter: drop-shadow(4px 4px 4px var(--slate950));
+	}
+
+	#windowTopDecoration {
+		background-color: var(--slate400);
+		border-top-left-radius: 5px;
+		border-top-right-radius: 5px;
+		height: 1.5em;
+		margin: 0px;
+		display: flex;
+		align-items: center;
+	}
+
+	.circleButton {
+		height: 1em;
+		width: 1em;
+		margin-left: 10px;
+		border-radius: 100%;
+	}
+
+	#exit {
+		background-color: #c9184a;
+	}
+
+	#min {
+		background-color: #ffd670;
+	}
+
+	#max {
+		background-color: #619b8a;
+	}
+</style>
