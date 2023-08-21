daacf70ee85f5bfad52476a4dabcd14bf23ddaed
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-08-21 22:01:33 +1000

    dev blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,2 +1,27 @@
-<div>blog page</div>
-<a href="/#/"> back to home </a>
+<script lang="ts">
+	import blog from '$lib/blog.json';
+	import MockBrowserWindow from '$lib/mockBrowserWindow/MockBrowserWindow.svelte';
+</script>
+
+<h1>Dev Blog</h1>
+
+{#each blog.posts as post}
+	<section class="blogPostContainer">
+		<MockBrowserWindow>
+			<div class="blogPost" slot="content">
+				<h2><i>{post.date}</i>: <strong>{post.title}</strong></h2>
+				<p>{post.post}</p>
+			</div>
+		</MockBrowserWindow>
+	</section>
+{/each}
+
+<style>
+	strong {
+		font-weight: 400;
+	}
+
+	.blogPost {
+		margin: 1em;
+	}
+</style>
